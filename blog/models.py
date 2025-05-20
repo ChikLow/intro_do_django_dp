c0aa4ef835d5b9c057d344bddb73d4555a1986a6
Roman <romanshtanhret@gmail.com>
2025-05-20 20:43:04 +0300

    project done

--- blog/models.py
+++ blog/models.py
@@ -5,16 +5,18 @@ from datetime import timedelta
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
-    bio = models.TextField(max_length=2500)
+    bio = models.TextField()
 
     def __str__(self):
         return self.name
     
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    content = models.TextField(max_length=2500)
+    content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, blank=True)
+
+
 
     def __str__(self):
         return self.title
