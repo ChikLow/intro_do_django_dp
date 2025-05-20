037dd5f0839488936ff2238987ff21aebc74d514
Roman <romanshtanhret@gmail.com>
2025-05-20 20:36:02 +0300

    continued project, an error

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_detail, posts_by_author
 
 urlpatterns = [
     path('', post_list, name='post_list'),
+    path('post/<int:post_id>/', post_detail, name='post_detail'),
+    path('author/<int:author_id>/', posts_by_author, name='posts_by_author'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.all().order_by('-published_date')
     return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    comments = post.comment_set.all()
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def posts_by_author(request, author_id):
+    author = get_object_or_404(author, pk=author_id)
+    posts = Post.objects.filter(author=author).all()
+    return render(request, 'blog/post_detail.html', {'author': author, 'posts': posts})
